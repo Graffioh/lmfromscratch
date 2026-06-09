080bf272ea814fe7c908dbbb115365812342d2e0
Graffioh <bregliascuola2002@gmail.com>
2026-06-09 08:11:59 +0200

    adjustments

--- assignment1-basics/cs336_basics/bpe_sequential.py
+++ assignment1-basics/cs336_basics/bpe_sequential.py
@@ -11,13 +11,13 @@ vocab[0] = "<|endoftext|>".encode("utf-8")
 for b in range(256):
     vocab[b + 1] = bytes([b])
 
+freq_table: Counter[tuple[bytes, ...]] = Counter()
+
 
 # simple, via whitespaces not gpt 2 regex
-def pretokenize(text: str) -> dict[tuple[bytes, ...], int]:
+def pretokenize(text: str):
     txt_split = re.split(r"\s+", text)
-    freq_table: Counter[tuple[bytes, ...]] = Counter()
 
-    # for each string in the corpus
     for s in txt_split:
         # convert the string into bytes
         bytes_str = s.encode("utf-8")
@@ -26,8 +26,6 @@ def pretokenize(text: str) -> dict[tuple[bytes, ...], int]:
         # for each bytes, we need to increment the counter
         bytes_tuple: tuple[bytes, ...] = tuple(bytes_list)
         freq_table[bytes_tuple] += 1
-
-    return freq_table
 
 
 def merge(
@@ -38,56 +36,64 @@ def merge(
 
     # merge max_merges times
     for _ in range(max_merges):
-        # build pair_table
-        pairs_table: defaultdict[tuple[bytes, ...], int] = defaultdict(int)
-        for k, v in freq_table.items():
+        # gather all the pairs to merge with respective cumulative count
+        pairs_to_merge: defaultdict[tuple[bytes, ...], int] = defaultdict(int)
+        for bytes_key, bytes_count in freq_table.items():
             pairs: list[tuple[bytes, bytes]] = []
-            for i in range(len(k) - 1):
-                pairs.append((k[i], k[i + 1]))
+            for i in range(len(bytes_key) - 1):
+                pairs.append((bytes_key[i], bytes_key[i + 1]))
 
             for p in pairs:
-                pairs_table[p] += v
+                pairs_to_merge[p] += bytes_count
 
-        # take the winning pair and merge it
-        max_pair = max(pairs_table.items(), key=lambda pair: (pair[1], pair[0]))
+        # we must check if there is any pair still mergeable
+        # (if pairs_table == 0, then it means we don't have pairs anymore, only single tokens hence nothing to merge)
+        if len(pairs_to_merge) == 0:
+            break
+
+        # take the winning pair (break ties with lexographically greater) and merge it
+        max_pair = max(pairs_to_merge.items(), key=lambda pair: (pair[1], pair[0]))
         winning_bytes = max_pair[0]
         merged_winning_bytes = winning_bytes[0] + winning_bytes[1]
 
         # find the winning bytes in the freq_table keys and whenever found, merge them
-        new_table: defaultdict[tuple[bytes, ...], int] = defaultdict(int)
-        for b_t_key, b_t_count in freq_table.items():
+        new_tmp_table: defaultdict[tuple[bytes, ...], int] = defaultdict(int)
+        for bytes_key, bytes_count in freq_table.items():
             new_key: list[bytes] = []
             i = 0
 
-            while i < len(b_t_key):
-                if i + 1 < len(b_t_key) and winning_bytes[0] == b_t_key[i] and winning_bytes[1] == b_t_key[i + 1]:
-                    # replace the pair with winning byte
+            while i < len(bytes_key):
+                if i + 1 < len(bytes_key) and winning_bytes[0] == bytes_key[i] and winning_bytes[1] == bytes_key[i + 1]:
                     new_key.append(merged_winning_bytes)
                     i += 2
                 else:
-                    new_key.append(b_t_key[i])
+                    new_key.append(bytes_key[i])
                     i += 1
 
             # update new_table with new {bytes key: freq count}
-            new_table[tuple(new_key)] += b_t_count
+            new_tmp_table[tuple(new_key)] += bytes_count
 
-        # freq_table is now new_table with new merged pairs!
-        freq_table = new_table
+        # freq_table is now the new table with new merged pairs!
+        freq_table = new_tmp_table
 
         # add the new entry in vocab
         new_vocab_words.append(merged_winning_bytes)
+
         merges.append((winning_bytes[0], winning_bytes[1]))
     return merges, new_vocab_words
 
 
-pretokenized_table = pretokenize(corpus_txt)
-merged_tokens, new_vocab_words = merge(pretokenized_table, 6)
+# split docs by EOT delimiter before pretokenize (so we avoid pretokenization across docs)
+txt_docs = re.split("<\\|endoftext\\|>", corpus_txt)
+for txt in txt_docs:
+    pretokenize(txt)
+
+merged_tokens, new_vocab_words = merge(freq_table, 6)
 
 cur_vocab_len = len(vocab)
 for nw in new_vocab_words:
     vocab[cur_vocab_len] = nw
     cur_vocab_len += 1
-
 
 print("+++++++++++++++++++++++++++")
 print("VOCAB: ", vocab)
